2fc76b4e9f8c44677c4ec8c73e58e08206eb1660
Derek Hall <94722790+Hderek22@users.noreply.github.com>
2022-05-24 23:01:33 -0400

    streamline coding format

--- Starter_Code 4/KaseiCoin.sol
+++ Starter_Code 4/KaseiCoin.sol
@@ -4,6 +4,8 @@ pragma solidity ^0.5.0;
 //    * `ERC20`
 //    * `ERC20Detailed`
 //    * `ERC20Mintable`
+
+
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Detailed.sol";
 import "https://github.com/OpenZeppelin/openzeppelin-contracts/blob/release-v2.5.0/contracts/token/ERC20/ERC20Mintable.sol";
